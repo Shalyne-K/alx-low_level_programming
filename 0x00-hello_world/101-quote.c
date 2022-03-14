64000b736e658b48c7ebee4c922a886a24ec8154
Shalyne-K <shalyne.kerubo@gmail.com>
2022-03-14 01:05:16 -0700

    Updated command with stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
  * Return: 1
  */
 int main(void)
-{	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+{	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
