67106c3ae3f1e9431a875699b9ec937730e29334
Shalyne-K <shalyne.kerubo@gmail.com>
2022-04-26 18:37:31 -0700

    Insert new node at a given position - alloc mem to new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 	if (idx == 0)
 	{
 		new_node->next = *head;
